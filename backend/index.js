c5a991cb579afff39b80ca83468db2d285ccd243
Shreyansh Dixit <krishnadixit0808@gmail.com>
2025-10-24 04:21:27 +0530

    updated frontend localhost

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,12 @@ const app = express();
 const PORT = 4000;
 
 // ------------------- Middlewares -------------------
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "https://documents-storage-website-front.onrender.com",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
