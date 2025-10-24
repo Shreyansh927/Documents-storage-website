118ecfbb37f0158c79932d59a744bdd407cb4ba7
Shreyansh Dixit <krishnadixit0808@gmail.com>
2025-10-24 17:08:50 +0530

    b

--- backend/authRoutes.js
+++ backend/authRoutes.js
@@ -206,7 +206,7 @@ const authRoutes = (db) => {
       });
     } catch (err) {
       console.error(err);
-      res.status(500).send("Login error");
+      res.status(500).json({ message: err.message });
     }
   });
 
