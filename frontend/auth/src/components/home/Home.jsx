d8b0dbfb9a709b8927dbf2e875e33ef7cfe46553
Shreyansh Dixit <krishnadixit0808@gmail.com>
2025-10-26 01:08:20 +0530

    gitt

--- frontend/auth/src/components/home/Home.jsx
+++ frontend/auth/src/components/home/Home.jsx
@@ -136,7 +136,11 @@ const Home = () => {
             }}
           >
             <h1 className="welcome-message">Welcome, {userName}!</h1>
-            <img src={`${BASE_URL}${profilePhoto}`} alt="profile-photo" />
+            <img
+              style={{ height: "60px", width: "60px", borderRadius: "50%" }}
+              src={`${BASE_URL}${profilePhoto}`}
+              alt="profile-photo"
+            />
           </div>
           <hr className="header-hr" />
           <div className="search-and-category-conatiner">
