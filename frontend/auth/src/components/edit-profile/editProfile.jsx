190a67b865755fb5cd31afb817a9238f09cb399d
Shreyansh Dixit <krishnadixit0808@gmail.com>
2025-10-26 01:56:23 +0530

    gitt

--- frontend/auth/src/components/edit-profile/editProfile.jsx
+++ frontend/auth/src/components/edit-profile/editProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./editProfile.css";
+import "./edit-profile.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Header from "../header/header";
