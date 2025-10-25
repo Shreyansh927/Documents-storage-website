3552c4344b43014a6b0f5af59f100d900a1bc9bc
Shreyansh Dixit <krishnadixit0808@gmail.com>
2025-10-26 01:54:21 +0530

    gitt

--- frontend/auth/src/components/edit-profile/editProfile.jsx
+++ frontend/auth/src/components/edit-profile/editProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import "./editProfile.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Header from "../header/header";
@@ -61,9 +61,6 @@ const EditProfile = () => {
       if (profileVideo && typeof profileVideo !== "string") {
         formData.append("profileVideo", profileVideo);
       }
-      if (profilePDF && typeof profilePDF !== "string") {
-        formData.append("profilePDF", profilePDF); //  append PDF if picked
-      }
 
       const { data } = await axios.put(
         "https://documents-storage-website-backend-2.onrender.com/api/auth/edit-profile",
@@ -95,98 +92,103 @@ const EditProfile = () => {
   return (
     <>
       <Header />
-      <div>
-        <h1>Edit Profile</h1>
-        <form onSubmit={editProfile}>
-          <label>Location</label>
-          <input
-            type="text"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-          />
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <label>Profile Image</label>
-          <input
-            type="file"
-            accept="image/*"
-            onChange={(e) => setProfileImage(e.target.files[0])}
-          />
-          {profileImage && (
-            <img
-              src={
-                typeof profileImage === "string"
-                  ? `https://documents-storage-website-backend-2.onrender.com${profileImage}`
-                  : URL.createObjectURL(profileImage)
-              }
-              alt="Profile Preview"
-              style={{
-                width: "400px",
-                height: "200px",
-                borderRadius: "10%",
-                objectFit: "cover",
-                marginTop: "40px",
-              }}
+      <div className="main-signup-container">
+        <div className="image-section">
+          <img src="" alt="edit-image" />
+        </div>
+        <div className="signup-section">
+          <h1>Edit Profile</h1>
+          <form onSubmit={editProfile}>
+            <label>Location</label>
+            <input
+              type="text"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
             />
-          )}
-          <label>Profile Video</label>
-          <input
-            type="file"
-            accept="video/*"
-            onChange={(e) => setProfileVideo(e.target.files[0])}
-          />
-          {profileVideo && (
-            <video
-              width="400"
-              height="200"
-              controls
-              style={{ borderRadius: "10px", marginTop: "20px" }}
-            >
-              <source
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label>Profile Image</label>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={(e) => setProfileImage(e.target.files[0])}
+            />
+            {profileImage && (
+              <img
                 src={
-                  typeof profileVideo === "string"
-                    ? `http://localhost:4000${profileVideo}`
-                    : URL.createObjectURL(profileVideo)
+                  typeof profileImage === "string"
+                    ? `https://documents-storage-website-backend-2.onrender.com${profileImage}`
+                    : URL.createObjectURL(profileImage)
                 }
-                type="video/mp4"
+                alt="Profile Preview"
+                style={{
+                  width: "400px",
+                  height: "200px",
+                  borderRadius: "10%",
+                  objectFit: "cover",
+                  marginTop: "40px",
+                }}
               />
-              Your browser does not support the video tag.
-            </video>
-          )}
-          <label>Profile PDF</label>
-          <label>Profile PDF</label>
-          <label>Profile PDF</label>
-          <input
-            type="file"
-            accept="application/pdf"
-            onChange={(e) => setProfilePDF(e.target.files[0])}
-          />
-          {profilePDF && (
-            <p style={{ marginTop: "20px" }}>
-              <a
-                href={
-                  typeof profilePDF === "string"
-                    ? `http://localhost:4000${profilePDF}`
-                    : URL.createObjectURL(profilePDF)
-                }
-                target="_blank"
-                rel="noopener noreferrer"
+            )}
+            <label>Profile Video</label>
+            <input
+              type="file"
+              accept="video/*"
+              onChange={(e) => setProfileVideo(e.target.files[0])}
+            />
+            {profileVideo && (
+              <video
+                width="400"
+                height="200"
+                controls
+                style={{ borderRadius: "10px", marginTop: "20px" }}
               >
-                {typeof profilePDF === "string"
-                  ? profilePDF.split("/").pop()
-                  : profilePDF.name}
-              </a>
-            </p>
-          )}
-          <button onClick={edit} type="submit">
-            Edit
-          </button>
-        </form>
+                <source
+                  src={
+                    typeof profileVideo === "string"
+                      ? `http://localhost:4000${profileVideo}`
+                      : URL.createObjectURL(profileVideo)
+                  }
+                  type="video/mp4"
+                />
+                Your browser does not support the video tag.
+              </video>
+            )}
+            <label>Profile PDF</label>
+            <label>Profile PDF</label>
+            <label>Profile PDF</label>
+            <input
+              type="file"
+              accept="application/pdf"
+              onChange={(e) => setProfilePDF(e.target.files[0])}
+            />
+            {profilePDF && (
+              <p style={{ marginTop: "20px" }}>
+                <a
+                  href={
+                    typeof profilePDF === "string"
+                      ? `http://localhost:4000${profilePDF}`
+                      : URL.createObjectURL(profilePDF)
+                  }
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {typeof profilePDF === "string"
+                    ? profilePDF.split("/").pop()
+                    : profilePDF.name}
+                </a>
+              </p>
+            )}
+            <button onClick={edit} type="submit">
+              Edit
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
